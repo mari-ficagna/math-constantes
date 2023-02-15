58925b2a7af98e20dcb0d1ae2b608d60137d9df8
Marieli Ficagna <marieli.ficagna@gmail.com>
2023-02-15 16:18:12 -0300

    first commit

---
+++ src/Aula5MathConstantes.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Mari (48) 99618-5728
+ */
+public class Aula5MathConstantes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float raio = 2.4f;
+        double comprimento = 2*raio*Math.PI;
+        System.out.println(comprimento);
+    }
+    
+}
